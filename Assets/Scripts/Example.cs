83f5922180d466a5eaf3434bd24cdb0ca75d8756
Andrii Sviatenko <105353259+AndriiSviatenko@users.noreply.github.com>
2024-08-25 06:46:26 +0300

    Add project

---
+++ Assets/Scripts/Example.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Patterns.EventBus;
+using Patterns.EventBus.Events;
+using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+    private bool _isAlive;
+
+    private void Update()
+    {
+        if (!_isAlive)
+        {
+            EventBus.Instance.Invoke(new DeadEvent<Example>(this));
+        }
+    }
+}
